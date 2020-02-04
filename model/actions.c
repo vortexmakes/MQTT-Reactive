3808340aa83dd2bb22518e86688eb61d54770a97
lf <francuccilea@gmail.com>
2020-02-04 12:10:44 -0300

    Added all actions and guards

--- model/actions.c
+++ model/actions.c
@@ -12,6 +12,9 @@
 /* ---------------------------- Local data types --------------------------- */
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
+static RKH_QUEUE_T deferQueue;
+static RKH_EVT_T *deferQueueStorage[MAX_SIZEOF_DEFERQUEUE];
+
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
 static void
@@ -38,6 +41,10 @@ connack_response_callback(enum MQTTConnackReturnCode return_code)
 void
 initialize(...)
 {
+    rkh_queue_init(&deferQueue, 
+                   (const void **)deferQueueStorage, 
+                   MAX_SIZEOF_DEFERQUEUE, NULL);
+
     mqtt_init(&me->client, 0, 
               me->sendbuf, sizeof(me->sendbuf), me->recvbuf, 
               sizeof(me->recvbuf), 0);
@@ -75,30 +82,280 @@ publish(...)
 }
 
 void 
-reconnectSync(SyncRegion *const me, RKH_EVT_T *pe)
+initRecvAll(SyncRegion *const me, RKH_EVT_T *pe)
+{
+	(void)pe;
+
+    mqtt_isReconnect(me->client);
+    mqtt_initRecvAll();
+}
+
+void 
+parseRecv(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+    ReceivedEvt *evt;
+
+    realMe = me->itsMQTTProt;
+    evt = RKH_DOWNCAST(ReceivedEvt, pe);
+
+    memcpy(realMe->client.recv_buffer.curr, evt->buf, evt->size);
+    localRecv.rv = evt->size;
+    mqtt_parseRecv(&realMe->client, &localRecv);
+}
+
+void 
+recvFail(SyncRegion *const me, RKH_EVT_T *pe)
 {
     MQTTProt *realMe;
 
 	(void)pe;
 
     realMe = me->itsMQTTProt;
-    RKH_SMA_POST_LIFO(RKH_UPCAST(RKH_SMA_T, realMe), 
-                      RKH_UPCAST(RKH_EVT_T, &evDeactivateObj), 
-                      realMe);
+    localRecv.rv = MQTT_ERROR_SOCKET_ERROR;
+    mqtt_recvFail(&realMe->client, &localRecv); /* an error occurred */
 }
 
-/* ............................. Entry actions ............................. */
-/* ............................. Exit actions .............................. */
-/* ................................ Guards ................................. */
-rbool_t 
-isReconnect(const RKH_SM_T *me, RKH_EVT_T *pe)
+void 
+parseError(SyncRegion *const me, RKH_EVT_T *pe)
 {
-    SyncRegion *realMe;
+    MQTTProt *realMe;
 
 	(void)pe;
 
-    realMe = RKH_DOWNCAST(SyncRegion, me);
-    return mqtt_isReconnect(&realMe->itsMQTTProt->client);
+    realMe = me->itsMQTTProt;
+    mqtt_parseError(&realMe->client, &localRecv);
+}
+
+void 
+noConsumed(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_noConsumed(&realMe->client, &localRecv);
+}
+
+void 
+handleRecvMsg(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_handleRecvMsg(&realMe->client, &localRecv);
+}
+
+void 
+recvMsgError(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_recvMsgError(&realMe->client, &localRecv);
+}
+
+void 
+cleanBuf(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_cleanBuf(&realMe->client, &localRecv);
+}
+
+void 
+initSendAll(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_initSendAll(&realMe->client, &localSend);
+}
+
+void 
+initSendAll(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_initSendAll(&realMe->client, &localSend);
+}
+
+void 
+endSendAll(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_endSendAll(&realMe->client);
+}
+
+void 
+sendOneMsg(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_sendOneMsg(&realMe->client, &localSend);
+}
+
+void 
+nextSend(SyncRegion *const me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    mqtt_nextSend(&localSend);
+}
+
+void 
+sendMsgFail(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    localSend.tmp = MQTT_ERROR_SOCKET_ERROR;
+    mqtt_sendMsgFail(&realMe->client, &localSend);
+}
+
+void 
+setMsgState(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    mqtt_setMsgState(&realMe->client, &localSend);
+}
+
+void 
+recall(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    rkh_sma_recall(me, &deferQueue);
+}
+
+void 
+defer(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    rkh_sma_defer(&deferQueue, pe);
+}
+
+/* ............................. Entry actions ............................. */
+void 
+recvAll(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    ConnectionTopic_publish(&evRecvObj, realMe);
+}
+
+void 
+sendAll(SyncRegion *const me, RKH_EVT_T *pe)
+{
+    MQTTProt *realMe;
+
+	(void)pe;
+
+    realMe = me->itsMQTTProt;
+    evSendObj.size = localSend.msg->size;
+    memcpy(evSendObj.buf, localSend.msg->start, localSend.msg->size);
+    ConnectionTopic_publish(&evSendObj, realMe);
+}
+
+/* ............................. Exit actions .............................. */
+/* ................................ Guards ................................. */
+rbool_t 
+isRecvBufFull(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isRecvBufFull(&localRecv);
+}
+
+rbool_t 
+isInitOk(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isInitOk(&localSend);
+}
+
+rbool_t 
+isThereMsg(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isThereMsg(&localSend);
+}
+
+rbool_t 
+isNotResend(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return localSend.resend == 0;
+}
+
+rbool_t 
+isSetMsgStateOk(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isSetMsgStateResult(&localSend);
+}
+
+rbool_t 
+isUnpackError(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isUnpackError(&localRecv);
+}
+
+rbool_t 
+isConsumed(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isConsumed(&localRecv);
+}
+
+rbool_t 
+isNotError(const RKH_SM_T *me, RKH_EVT_T *pe)
+{
+	(void)me;
+	(void)pe;
+
+    return mqtt_isNotError(&localRecv);
 }
 
 /* ------------------------------ File footer ------------------------------ */
